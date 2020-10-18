58cc926babbcd6c836b86af16e8c4578322c59ac
pan0sSt <p.stavrinakis@gmail.com>
2020-10-18 12:22:38 -0400

    Read newIP File & scan

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 
 import scapy.all as scapy  # handle tasks like scanning and network discovery
 import time  # use sleep() for delays
-import argparse  # get values as arguments
 
 
 # function that returns MAC address of selected IP
@@ -65,8 +64,9 @@ class ArpSpoof:
                 f.write("0")  # disable ip forwarding
 
 
-# a tuple containing all the default router gateways
-router_default = ('10.90.90.90', '192.168.0.50', '10.1.1.1', '192.168.0.3', '192.168.0.1', '192.168.1.99',
+# ------------------------------POSSIBLE ROUTER IP------------------------------
+# a set containing all the default router gateways
+router_default = {'10.90.90.90', '192.168.0.50', '10.1.1.1', '192.168.0.3', '192.168.0.1', '192.168.1.99',
                   '192.168.10.50', '192.168.254.254', '192.168.0.10', '192.168.123.254', '10.0.0.138', '10.0.0.2',
                   '192.168.3.1', '192.168.2.1', '192.168.100.100', '192.168.223.100', '192.168.0.227', '192.168.1.254',
                   '192.168.0.254', '192.168.100.1', '192.168.11.1', '192.168.0.101', '10.0.1.1', '192.168.30.1',
@@ -74,15 +74,29 @@ router_default = ('10.90.90.90', '192.168.0.50', '10.1.1.1', '192.168.0.3', '192
                   '192.168.251.1', '192.168.10.100', '192.168.16.1', '192.168.15.1', '192.168.86.1', '192.168.10.10',
                   '192.168.1.10.1', '10.1.10.1', '192.168.20.1', '192.168.1.1', '192.168.50.1', '192.168.0.100',
                   '192.168.1.10', '192.168.1.20', '192.168.1.210', '192.168.10.1', '192.168.62.1', '200.200.200.5',
-                  '192.168.4.1', '10.0.0.1', '192.168.1.100', '192.168.8.1', '192.168.0.30')
+                  '192.168.4.1', '10.0.0.1', '192.168.1.100', '192.168.8.1', '192.168.0.30'}
+
+# Update the router_default with the new IPs
+try:
+    with open('/root/PycharmProjects/TheBoss/newIPs', 'r') as newIPsFile:
+        update_IPs = [ip.strip('\n\r') for ip in newIPsFile.readlines()]
+        router_default.update(update_IPs)
+    newIPsFile.close()
+except FileNotFoundError:
+    print("[!] newIPs file doesn't exist")
+# ------------------------------------------------------------------------------
+
+# scan network for IPs
+scapy.arping('10.0.2.0/16')
+
 scan_list = ['10.0.2.8', '10.0.2.1']
 
-for curr_ip in scan_list:
-    if curr_ip in router_default:
-        print('{}'.format(curr_ip))
-    else:
-        print('not in')
-target = input('Your target IP: ')
-source = input('Your source IP: ')
-arp_spoofing = ArpSpoof(target, source)
-arp_spoofing.run()
+# for curr_ip in scan_list:
+#     if curr_ip in router_default:
+#         print('{}'.format(curr_ip))
+#     else:
+#         print('not in')
+# target = input('Your target IP: ')
+# source = input('Your source IP: ')
+# arp_spoofing = ArpSpoof(target, source)
+# arp_spoofing.run()
