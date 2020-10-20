eb0411df1e1be5244b7fdf7b71036b928d9be5ce
pan0sSt <p.stavrinakis@gmail.com>
2020-10-20 11:12:16 -0400

    Menu Init

--- main.py
+++ main.py
@@ -4,7 +4,28 @@ import scapy.all as scapy  # handle tasks like scanning and network discovery
 import subprocess  # function for shell commands
 import re  # regular expressions
 import time  # use sleep() for delays
-from sys import platform  # info about the os
+import sys  # info about the os
+
+
+# function that update the router_default with the new IPs
+def update_routers(defaults):
+    try:
+        print('[+] Reading file: newIPs...')
+        time.sleep(0.1)
+        with open('/root/PycharmProjects/TheBoss/newIPs', 'r') as newIPsFile:
+            print("[+] Updating router's IP list...")
+            time.sleep(0.1)
+            update_ips = [ip.strip('\n\r') for ip in newIPsFile.readlines()]
+            defaults.update(update_ips)
+            print("[+] Done updating. Closing file...")
+            time.sleep(0.1)
+        newIPsFile.close()
+        print("[+] File closed.")
+        time.sleep(0.1)
+        return defaults
+    except FileNotFoundError:
+        print("[!] newIPs file doesn't exist")
+        return defaults
 
 
 # function that returns a list with responses from a broadcast
@@ -19,8 +40,6 @@ def broadcast(ip):
 
 # function that scans network IPs
 def network_scanner():
-    print('[+] Scanning Network...')
-    time.sleep(0.1)
     if SYS_PLATFORM == "linux":
         print('[+] Capturing IPv4 Address...')
         time.sleep(0.1)
@@ -28,9 +47,12 @@ def network_scanner():
         ipv4_search_result = re.search(r"\b(\d+\.){3}\d+/\d+\b", eth0_result)
         if ipv4_search_result:
             try:
+                # in order to scan, the default IP needs format 'x.x.x.0/x'
                 dot_index = (len(ipv4_search_result.group(0)) - 1) - ipv4_search_result.group(0)[::-1].index('.')
                 slash_index = ipv4_search_result.group(0).index('/')
                 scan_ip = ipv4_search_result.group(0)[:dot_index + 1] + '0' + ipv4_search_result.group(0)[slash_index:]
+                print('[+] Scanning Network...')
+                time.sleep(0.1)
                 return [ip[1].psrc for ip in broadcast(scan_ip)]  # returns the list with the IPs
             except ValueError:
                 print("[!] '.' or '/' not found in IPv4 string")
@@ -38,7 +60,7 @@ def network_scanner():
         else:
             print("[-] Could not read IPv4 Address.")
             return
-    elif SYS_PLATFORM == "win32":
+    elif SYS_PLATFORM == "win32":  # TODO //////////////////////////////////
         example = 128
         if example == 0:
             num_of_zeros = 8
@@ -47,6 +69,33 @@ def network_scanner():
             num_of_zeros = len(binary_form) - len(binary_form.rstrip('0'))
     else:
         return
+
+
+# function that searches the scanned list for router's IP
+def search_router_ip(scan_list, routers):
+    print('[+] Searching for router IP...')
+    time.sleep(0.1)
+    for curr_ip in scan_list:
+        if curr_ip in routers:
+            print('[+] Router IP found: {}'.format(curr_ip))
+            time.sleep(0.1)
+            router = curr_ip
+            scan_list.remove(curr_ip)
+            return router
+    print('[-] Search failed! Returning whole list of IPs.')
+    return
+
+
+# function that returns target, source IPs from input
+def manual_ip_input(scan_list):
+    print("IP Table")
+    print("--------")
+    for ip in scan_list:
+        print(ip)
+    print("--------")
+    trgt = input('Your target IP: ')
+    src = input('Your source IP: ')
+    return trgt, src
 
 
 # function that returns MAC address of selected IP
@@ -105,9 +154,11 @@ class ArpSpoof:
                 f.write("0")  # disable ip forwarding
 
 
-SYS_PLATFORM = platform  # os of the current machine
-
+SYS_PLATFORM = sys.platform  # os of the current machine
+command = ''
+scan_result = []
 # ------------------------------POSSIBLE ROUTER IP------------------------------
+router_ip = ''
 # a set containing all the default router gateways
 router_default = {'10.90.90.90', '192.168.0.50', '10.1.1.1', '192.168.0.3', '192.168.0.1', '192.168.1.99',
                   '192.168.10.50', '192.168.254.254', '192.168.0.10', '192.168.123.254', '10.0.0.138', '10.0.0.2',
@@ -118,43 +169,62 @@ router_default = {'10.90.90.90', '192.168.0.50', '10.1.1.1', '192.168.0.3', '192
                   '192.168.1.10.1', '10.1.10.1', '192.168.20.1', '192.168.1.1', '192.168.50.1', '192.168.0.100',
                   '192.168.1.10', '192.168.1.20', '192.168.1.210', '192.168.10.1', '192.168.62.1', '200.200.200.5',
                   '192.168.4.1', '10.0.0.1', '192.168.1.100', '192.168.8.1', '192.168.0.30'}
-
-# Update the router_default with the new IPs
-try:
-    print('[+] Reading file: newIPs...')
-    time.sleep(0.1)
-    with open('/root/PycharmProjects/TheBoss/newIPs', 'r') as newIPsFile:
-        print("[+] Updating router's IP list...")
-        time.sleep(0.1)
-        update_IPs = [ip.strip('\n\r') for ip in newIPsFile.readlines()]
-        router_default.update(update_IPs)
-        print("[+] Done updating. Closing file...")
-        time.sleep(0.1)
-    newIPsFile.close()
-    print("[+] File closed.")
-    time.sleep(0.1)
-except FileNotFoundError:
-    print("[!] newIPs file doesn't exist")
+router_default = update_routers(router_default)
 # ------------------------------------------------------------------------------
 
-scan_result = network_scanner()
-
-if scan_result:
-    print('[+] Searching for router IP...')
-    time.sleep(0.1)
-    for curr_ip in scan_result:
-        if curr_ip in router_default:
-            print('[+] Router IP found: {}'.format(curr_ip))
+# --------------------------------MENU INTERFACE--------------------------------
+while True:
+    print("Insert your command:")
+    command = input(">> ")
+    try:
+        if command == "exit()":
+            print("[+] Cya later Boss!")
             time.sleep(0.1)
-            router_ip = curr_ip
-            scan_result.remove(curr_ip)
-            break
+            sys.exit()
+
+        elif command == "scan()":
+            scan_result = network_scanner()
+            if scan_result:
+                router_ip = search_router_ip(scan_result, router_default)
+            else:
+                print('[-] Network scan failed.')
+
+        elif command == "arpspoof()":
+            if scan_result:
+                if router_ip:  # TODO ///////////////////////
+                    print("[+] Router IP found...")
+                    time.sleep(0.1)
+                    print("[+] Start auto MITM")
+                    time.sleep(0.1)
+                else:
+                    print("[-] Router IP not found. Choose manually..")
+                    time.sleep(0.1)
+                    target, source = manual_ip_input(scan_result)
+                    while target not in scan_result or source not in scan_result:
+                        print("[-] IPs not found in list. Try again")
+                        time.sleep(0.1)
+                        target, source = manual_ip_input(scan_result)
+                    arp_spoofing = ArpSpoof(target, source)
+                    arp_spoofing.run()
+            else:
+                print("[-] Network IPs not defined. Please scan() first.")
+                time.sleep(0.1)
+
+        elif command == "update()":
+            router_default = update_routers(router_default)
+
+        elif command == "help()":
+            print("----------------------------------")
+            print("COMMANDS      DESCRIPTION")
+            print("----------------------------------")
+            print("scan()        Network scanning")
+            print("arpspoof()    MITM Attack")
+            print("exit()        Exit the app")
+            print("----------------------------------")
+
         else:
-            pass
-else:
-    print('[-] Network scan failed.')
-print(scan_result)
-# target = input('Your target IP: ')
-# source = input('Your source IP: ')
-# arp_spoofing = ArpSpoof(target, source)
-# arp_spoofing.run()
+            print('[-] Command not found. Type help() to show commands.')
+
+    except Exception:
+        print('[-] Error during command execution.')
+# ------------------------------------------------------------------------------
